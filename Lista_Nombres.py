6ef31ca936fefb06c731273ade7a4b537dcc1280
Criss0211 <cri.gutierrezm@duocuc.cl>
2025-07-07 14:30:54 -0400

    Listado Participantes

---
+++ Lista_Nombres.py
@@ -0,0 +1,10 @@
+print("Examen Transversal Programacion y redes Virtualizadas")
+
+participantes = [
+    "Sebastian Vega",
+    "Cristhofer Gutierrez"
+]
+
+print("Participantes del Examen:")
+for nombre in participantes:
+    print(f"-{nombre}")
